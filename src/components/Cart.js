b8209c5ca17bec01cdf3952cc1f0196654793d4e
Oumar Yanni <oumaryanni@gmail.com>
2024-10-18 00:14:12 +0000

    Styling the cart components

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,5 @@
+import "../styles/Cart.css";
+
 function Cart() {
   const plants = [
     { name: "Monstera", price: 8 },
@@ -8,7 +10,7 @@ function Cart() {
   const total = plants.reduce((acc, plant) => acc + plant.price, 0);
 
   return (
-    <>
+    <div className="lmj-cart">
       <ul>
         {plants.map((plant, index) => (
           <li key={index}> {`${plant.name} : ${plant.price}`}</li>
@@ -16,7 +18,7 @@ function Cart() {
       </ul>
 
       <h2> Total du Panier : {total}</h2>
-    </>
+    </div>
   );
 }
 
